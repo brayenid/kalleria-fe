efac82f6a932fa26ab48c575f549e4432d512abc
brayenid <bfenomenal@gmail.com>
2023-08-19 12:01:47 +0800

    fix: presensi kelas, admin db index

--- pages/admin/dashboard/index.vue
+++ pages/admin/dashboard/index.vue
@@ -5,13 +5,14 @@ definePageMeta({
 })
 
 const { $axiosAuth: axios } = useNuxtApp()
+const router = useRouter()
 
 const transaksiTotal = ref(0)
 const usersTotal = ref(0)
 const kelasTotal = ref(0)
 
 onMounted(async () => {
-  const responseTransaksiTotal = (await axios.get('/transaksi?pageSize=1')).data.data.total
+  const responseTransaksiTotal = (await axios.get('/transaksi?pageSize=1&status=pending')).data.data.total
   transaksiTotal.value = responseTransaksiTotal
 
   const responseUsersTotal = (await axios.get('/users?pageSize=1')).data.data.total
@@ -24,7 +25,7 @@ onMounted(async () => {
 
 <style lang="postcss" scoped>
 .container-item {
-  @apply shadow rounded p-6 flex flex-col justify-center items-center;
+  @apply shadow rounded p-6 flex flex-col justify-center items-center cursor-pointer transition-all;
 }
 .item {
   @apply flex gap-4 text-6xl font-bold;
@@ -37,19 +38,19 @@ onMounted(async () => {
   <div>
     <p class="mb-6 font-light text-lg">Halo Admin,</p>
     <div class="grid gap-8 lg:grid-cols-3 mb-6">
-      <div class="container-item bg-purple-100 text-purple-500">
+      <div @click="router.push('/admin/dashboard/kelas')" class="container-item bg-purple-100 hover:bg-purple-200 text-purple-500">
         <p class="item"><IconsLibrary class="icon !text-purple-500" /> {{ kelasTotal }}</p>
         <p>Kelas yang terbuka untuk siswa</p>
       </div>
 
-      <div class="container-item bg-blue-100 text-blue-500">
+      <div @click="router.push('/admin/dashboard/siswa')" class="container-item bg-blue-100 hover:bg-blue-200 text-blue-500">
         <p class="item"><IconsGroup class="icon !text-blue-500" />{{ usersTotal }}</p>
         <p>Siswa yang telah bergabung</p>
       </div>
 
-      <div class="container-item bg-green-100 text-green-500">
-        <p class="item"><IconsMoney class="icon !text-green-500" />{{ transaksiTotal }}</p>
-        <p>Transaksi yang telah dilakukan</p>
+      <div @click="router.push('/admin/dashboard/transaksi')" class="container-item bg-red-100 hover:bg-red-200 text-red-500">
+        <p class="item"><IconsMoney class="icon !text-red-500 text-center" />{{ transaksiTotal }}</p>
+        <p>Transaksi harus ditinjau (pending)</p>
       </div>
     </div>
   </div>
--- pages/admin/presensi/index.vue
+++ pages/admin/presensi/index.vue
@@ -93,6 +93,12 @@ const submitForm = async (e) => {
 }
 </script>
 
+<style lang="postcss" scoped>
+.attendance input {
+  @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
+}
+</style>
+
 <template>
   <section class="bg-gray-50 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -103,54 +109,23 @@ const submitForm = async (e) => {
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Presensi</h1>
-          <form class="space-y-4 md:space-y-6" @submit.prevent="submitForm">
+          <form class="attendance space-y-4 md:space-y-6" @submit.prevent="submitForm">
             <div class="relative">
               <label for="kelasId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID Kelas</label>
-              <input
-                ref="kelasListInputEl"
-                v-model="kelasListInput"
-                @input="debounceSearchKelas(kelasListInput)"
-                type="text"
-                name="kelasId"
-                id="kelasId"
-                placeholder="Masukan ID Kelas"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-                autocomplete="off"
-              />
+              <input ref="kelasListInputEl" v-model="kelasListInput" @input="debounceSearchKelas(kelasListInput)" type="text" name="kelasId" id="kelasId" placeholder="Masukan ID Kelas" required autocomplete="off" />
               <div v-click-outside="resetKelasList" v-if="kelasList.length" class="suggestion-container">
                 <ul class="flex flex-col text-sm">
-                  <li class="suggestion" @click="setKelasListInput(kelas.id)" v-for="kelas in kelasList" :key="kelas.id">{{ kelas.namaKelas }} ({{ kelas.id }})</li>
+                  <li class="suggestion" @click="setKelasListInput(kelas.id)" v-for="kelas in kelasList" :key="kelas.id">{{ kelas.namaKelas }}</li>
                 </ul>
               </div>
             </div>
             <div>
               <label for="jumlahPertemuan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah Pertemuan</label>
-              <input
-                type="number"
-                name="jumlahPertemuan"
-                id="jumlahPertemuan"
-                placeholder="Masukan Jumlah Pertemuan"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required=""
-                value="1"
-              />
+              <input type="number" name="jumlahPertemuan" id="jumlahPertemuan" placeholder="Masukan Jumlah Pertemuan" required="" value="1" />
             </div>
             <div class="relative">
               <label for="userId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID Siswa</label>
-              <input
-                ref="userInputEl"
-                v-model="userInput"
-                @input="debounceSearch(userInput)"
-                type="text"
-                name="userId"
-                id="userId"
-                placeholder="Masukan ID Siswa"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-                autofocus
-                autocomplete="off"
-              />
+              <input ref="userInputEl" v-model="userInput" @input="debounceSearch(userInput)" type="text" name="userId" id="userId" placeholder="Masukan ID Siswa" required autofocus autocomplete="off" />
               <div v-click-outside="resetUserList" v-if="userList.length" class="suggestion-container">
                 <ul class="flex flex-col text-sm">
                   <li class="suggestion" @click="setUserInput(user.id)" v-for="user in userList" :key="user.id">{{ user.nama }}</li>
